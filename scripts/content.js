2513be8f95b450108cda61b470c132d9c417bcc9
0x7b <0x7b123@gmail.com>
2025-09-25 11:04:22 +0900

    メニューの表示7割完成

--- scripts/content.js
+++ scripts/content.js
@@ -45,34 +45,96 @@ function loadCourseInfo(allDivs){
 function deleteWell() {
   for (const element of courseInfoList){
     const div = document.getElementById(element.id);
-    const header = div.querySelector('.media-heading')
+    const header = div.querySelector('.media-heading');
     if (element.fullName　!== element.callName) {
           header.innerHTML = header.textContent.replaceAll(element.fullName, element.callName);
-        }
-    if (element.state === false){
-        div.parentElement.style.display = "none";
-  } 
+      }
+    div.parentElement.style.display = (element.state === false) ? "none" : "";
     }
   return false; 
 }
 
-function menu(){
-  const btn = document.createElement('button');
-  btn.id ='btn'
-  btn.textContent = '表示メニュー'
-  const content = document.querySelector('.content-header')
-  content.prepend(btn)
+function menu() {
+    const btn = document.createElement('button');
+    btn.id = 'btn'
+    btn.textContent = '表示メニュー'
+    const content = document.querySelector('.content-header')
+    content.prepend(btn)
 
-  const menuBoard = document.createElement('div');
-  menuBoard.id = 'menuboard';
-  const mainElement = document.querySelector('body');
-  mainElement.prepend(menuBoard);
-  menuBoard.textContent = TARGET
+    const menuBoard = document.createElement('div');
+    menuBoard.id = 'menuboard';
+    const mainElement = document.querySelector('body');
+    mainElement.prepend(menuBoard);
 
-  btn.addEventListener('click', (e) =>{
-    menuBoard.style.display = 'flex';
-  })
+    fetch(chrome.runtime.getURL('scripts/setting.html'))
+        .then(response => response.text())
+        .then(data => {
+            menuBoard.innerHTML = data;
+            const menuContent = document.querySelector('#menu-content');
+
+            for (const info of courseInfoList) {
+                const element = document.createElement('div');
+                element.className = 'settingItem';
+
+                const textItem = document.createElement('div');
+                textItem.className = 'text-item'; 
+
+                const courseName = document.createElement('div');
+                courseName.textContent = info.fullName;
+                courseName.className = 'Item-title';
+                textItem.appendChild(courseName);
+
+                const callName = document.createElement('div');
+                callName.textContent = '名前変更';
+                callName.className = 'Item-callName sub-Item';
+                textItem.appendChild(callName);
+
+                const courseState = document.createElement('div');
+
+                
+                if (info.state === true) {
+                    courseState.textContent = ' 表示 ';
+                    courseState.style.backgroundColor = 'black'
+                    courseState.style.color = 'white'
+                } else {
+                    courseState.textContent = '非表示';
+                }
+
+                courseState.addEventListener('click', (e) =>{
+                  changeState(courseState, info);
+                })
+
+
+                courseState.className = 'Item-courseState sub-Item';
+
+                
+                element.appendChild(textItem);
+                element.appendChild(courseState); 
+                
+                menuContent.appendChild(element);
+            }
+
+            btn.addEventListener('click', (e) => {
+                menuBoard.style.display = 'flex';
+            })
+        })
 }
+
+function changeState(element, info){
+  if (info.state === false) {
+        element.textContent = ' 表示 ';
+        element.style.backgroundColor = 'black'
+        element.style.color = 'white';
+        info.state = true;
+    } else {
+        element.textContent = '非表示';
+        element.style.backgroundColor = 'white'
+        element.style.color = 'black';
+        info.state = false;
+    }
+    deleteWell();
+}
+  
 
 const mainElement = document.querySelector('main');
 if (mainElement) {
